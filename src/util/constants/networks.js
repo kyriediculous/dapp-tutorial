cff84d35bcd618de198667de7b3760871c8cb4cf
kyriediculous <vergauwennico@gmail.com>
2018-03-08 17:42:24 +0100

    clear

--- src/util/constants/networks.js
+++
@@ -1,9 +0,0 @@
-export const NETWORKS = {
-  '1': 'Main Net',
-  '2': 'Deprecated Morden test network',
-  '3': 'Ropsten test network',
-  '4': 'Rinkeby test network',
-  '42': 'Kovan test network',
-  '4447': 'Truffle Develop Network',
-  '5777': 'Ganache Blockchain'
-}
